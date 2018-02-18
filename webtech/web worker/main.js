6ca41efe469d971d5b67f5645b6c9494e7d05c43
Ambuj Kumar <itsambuja@gmail.com>
2018-02-18 22:36:05 +0530

    factorial table added

--- webtech/web worker/main.js
+++ webtech/web worker/main.js
@@ -4,12 +4,23 @@ function process() {
         const myworker = new window.Worker('worker.js');
 
         var v = document.getElementById('num').value;
+        
+        // send input value to worker
+        function create(){
+            for (i=0; i<= v; i++){
+                myworker.postMessage(i);    
+            }
+        }
 
-        myworker.postMessage(v);
-
+        // receive calculated value from worker
+        document.getElementById('table').innerHTML = '';
+        var count = 0;
         myworker.onmessage = function(e) {
             document.getElementById('result').innerHTML = e.data;
+            a = document.getElementById('table');
+            a.innerHTML += '<br>' + count++ +'!='+ e.data;
         }
+        create();
     }
     else
         alert("Your browser doesn't support web worker!!");

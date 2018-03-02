078fb59df3bcf6cafa38a8104f29ccfbf9456ad4
AmbujaAK <itsambuja@gmail.com>
2018-03-02 13:41:33 +0530

    file edited

--- webtech/JSONandAJAX/main.js
+++ webtech/JSONandAJAX/main.js
@@ -4,14 +4,14 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+  ourRequest.open('GET','https://raw.githubusercontent.com/AmbujaAK/practice/master/webtech/JSONandAJAX/json/friendlist-' + pageCounter + '.json');
   ourRequest.onload = function () {
     var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
   };
   ourRequest.send();
   pageCounter++;
-  if (pageCounter == 4) {
+  if (pageCounter == 3) {
     btn.style.display = (btn.style.display == "none") ? "block" : "none";
   }
 });
@@ -19,7 +19,7 @@ btn.addEventListener("click", function() {
 function renderHTML(data) {
   var htmlString = "";
   for (var i = 0; i < data.length; i++) {
-    htmlString = "<p>" + data[i] + "</p>";
+    htmlString = "<p> Name : " + data[i].name + "</p>";
+    subject.insertAdjacentHTML('beforeend', htmlString);
   }
-  subject.insertAdjacentHTML('beforeend', htmlString);
 }

6e03daf7994c898d3790259f8524ebcf951dea48
AmbujaAK <itsambuja@gmail.com>
2018-03-24 02:33:21 +0530

    dept drop down added , and working a little bit

--- wt2/q18/script.js
+++ wt2/q18/script.js
@@ -1,5 +1,7 @@
 function display() {
   var roll = document.getElementById('text').value;
+  var dept = document.getElementById('dept');
+  var d = parseInt(document.getElementById('dept').value);
   res = "";
   var students = [
     ["Ambuj Kumar","74","IT"],
@@ -8,23 +10,43 @@ function display() {
     ["Manisha Mandal","43","IT"],
     ["Deepa Kumari","80","IT"],
     ["Choden Lama","67","IT"],
-    ["Payel Roy","2","IT"],
+    ["Ritem Bera","21","INSTRUMENTATION"],
+    ["Shrishty Kumari","22","PRINTING"],
+    ["Priyanka Nag","23","PRINTING"],
+    ["Aman Aggrawal","4","CONSTRUCTION"],
+    ["Priya Kumari","25","POWER"],
+    ["Prasant Rajput","26","POWER"],
+    ["Pratysh","28","CONSTRUCTION"],
+    ["Saurav","33","INSTRUMENTATION"],
   ];
-  var size = 7;
+  //var size = 7;
   //console.log(size);
   s_list = [];
   res += '<ul>';
+  //var flag;
   for (var i=0;i<students.length;i++){
-    res += '<li> <h6> Name :'+ students[i][0] + '<h6></li>';
-    res += '<li> <h6> Roll No. :'+ students[i][1] + '<h6></li>';
-    res += '<li> <h6> Department :'+ students[i][2] + '<h6></li>';
-    s_list.push(res);
+    if(roll == students[i][1]){
+      //flag = 1;
+      res += '<li> <h6> Name :'+ students[i][0] + '<h6></li>';
+      res += '<li> <h6> Roll No. :'+ students[i][1] + '<h6></li>';
+      res += '<li> <h6> Department :'+ students[i][2] + '<h6></li>';
+    }
+    else if (dept.options[dept.selectedIndex].text == students[i][2]) {
+      console.log(dept.options[dept.selectedIndex].tex);
+      //flag = 1;
+      res += '<li> <h6> Name :'+ students[i][0] + '<h6></li>';
+      res += '<li> <h6> Roll No. :'+ students[i][1] + '<h6></li>';
+      res += '<li> <h6> Department :'+ students[i][2] + '<h6></li>';
+    }
+      document.getElementById('content').innerHTML = res;
+    //s_list.push(res);
     //s_list.push(students[i]);
   }
   res+='</ul>';
   console.log(s_list);
+/*
   for (var i=0;i<students.length;i++){
-    if(students[i][1] == roll){
+    if(roll == students[i][1]){
       document.getElementById('content').innerHTML = s_list[i];
       break;
     }
@@ -32,4 +54,5 @@ function display() {
       document.getElementById('content').innerHTML = 'No Data Available';
     }
   }
+  */
 }

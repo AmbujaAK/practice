1730a1600e61d4aec359b379a7d021240616332f
AmbujaAK <itsambuja@gmail.com>
2018-04-11 17:56:40 +0530

    a line removed

--- wt2/q17/main.js
+++ wt2/q17/main.js
@@ -1,20 +1,17 @@
 function start(name) {
   if (window.WebSocket) {
-	  //alert('ws://' + window.location.host + '/ambuj/q17/' + name);
     socket = new WebSocket('ws://' + window.location.host + '/git/chat/' + name);
-	
   } else {
     show('Error: Your browser does not support WebSocket.');
     return;
   }
   socket.onopen = function () {
-	//alert('a');
     show('WebSocket connection opened.');
-    ele = document.getElementById('chat');
+    var ele = document.getElementById('chat');
     ele.onkeydown = function(event) {
       if (event.keyCode == 13) {
         socket.send(ele.value);
-        ele.value = ''
+        ele.value = '';
       }
     };
   };

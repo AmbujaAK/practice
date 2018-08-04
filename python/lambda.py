93eaaf0ccc1a5731ce33c3dbab34563c59085711
Ambuj Kumar <itsambuja@gmail.com>
2018-08-04 18:01:15 +0530

    file removed

--- python/lambda.py
+++
@@ -1,3 +0,0 @@
-arr = [int(item) for item in input().split()]
-
-print(list(map((lambda x: x ** 2),arr)))
--- python/use_enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-class Month(Enum):
-    jan = 1
-    feb = 2
-print(Month.jan.name)
-print(Month.jan.value)

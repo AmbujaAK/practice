eede41a6e768d52816a0a18e6903ea28e7845810
Ambuj Kumar <itsambuja@gmail.com>
2018-09-10 17:44:36 +0530

    file modified

--- python/absdiff.py
+++ python/absdiff.py
@@ -9,6 +9,12 @@ print('List 2 :',new_lst)
 
 dic = {}
 for i,j in zip(lst,new_lst):
-    dic.keys() = i
-    dic.values() = j
+    dic[i] = j
+
+#del dic[1]
+list1 = dic.keys()
+list2 = dic.values()
+
 print(dic)
+print(list1)
+print(list2)

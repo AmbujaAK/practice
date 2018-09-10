02d3ca05c714dafa10e0b2d788765aab7dad3989
Ambuj Kumar <itsambuja@gmail.com>
2018-09-10 18:36:04 +0530

    file modified

--- ds/stack/checkParenthesis.cpp
+++ ds/stack/checkParenthesis.cpp
@@ -4,37 +4,37 @@
 using namespace std;
 
 int main(){
-  cout<<"\nEnter the expression only containg {},[] or () : ";
-  string ex;
-  cin>>ex;
+    cout<<"\nEnter the expression only containg {},[] or () : ";
+    string ex;
+    cin>>ex;
 
-  stack<char> s;
-  int count=0;
-  for(int i=0;i<ex.length();i++){
-    if(ex[i]=='(' || ex[i]=='{' || ex[i]=='['){
-      s.push(ex[i]);
-      count++;
-      //cout<<"stack : "<<s.top()<<"\ncount : "<<count<<endl;
+    stack<char> s;
+    int count=0;
+    for(int i=0;i<ex.length();i++){
+		if(ex[i]=='(' || ex[i]=='{' || ex[i]=='['){
+			s.push(ex[i]);
+			count++;
+			//cout<<"stack : "<<s.top()<<"\ncount : "<<count<<endl;
+		}
+		else {
+			//cout<<"\nnext :"<<ex[i];
+			if(ex[i]=='}' && s.top()=='{'){
+				s.pop();
+				//cout<<"\ncount : "<<count;
+				count--;
+			}else if(ex[i]==')' && s.top()=='('){
+				s.pop();
+				count--;
+			} else if(ex[i]==']' && s.top()=='['){
+				s.pop();
+				count--;
+			}
+			else{
+				cout<<"not balanced"<<endl;
+				break;
+			}
+		}
     }
-    else {
-      //cout<<"\nnext :"<<ex[i];
-      if(ex[i]=='}' && s.top()=='{'){
-        s.pop();
-        //cout<<"\ncount : "<<count;
-        count--;
-      }else if(ex[i]==')' && s.top()=='('){
-        s.pop();
-        count--;
-      } else if(ex[i]==']' && s.top()=='['){
-        s.pop();
-        count--;
-      }
-      else{
-        cout<<"not balanced"<<endl;
-        break;
-      }
-    }
-  }
-  if(count==0)
-    cout<<"balanced"<<endl;
+    if(count == 0)
+      	cout<<"balanced"<<endl;
 }

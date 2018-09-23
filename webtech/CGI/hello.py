5bbacec1de6f11fdcdf132de4ceba29997268499
Ambuj Kumar <itsambuja@gmail.com>
2018-09-23 13:35:30 +0530

    cgi using python added

--- webtech/CGI/hello.py
+++ webtech/CGI/hello.py
@@ -1,1 +1,8 @@
-#!
+#!C:/Users/Ambuj Kumar/AppData/Loca/Programs/Python/Python36-32/python.exe
+print("Content-type: text/html")
+print("<html>\n")
+print("<head><title>CGI using Python</title></head>\n")
+print("<body>\n")
+print("<b>Hello,world</b>\n")
+print("</body>\n")
+print("</html>\n")

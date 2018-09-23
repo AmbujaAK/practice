e59b50693ab3d19b11f27a28e7ddca842ee75d9a
Ambuj Kumar <itsambuja@gmail.com>
2018-09-23 13:19:15 +0530

    database added

--- wt2/q25/q25.sql
+++ wt2/q25/q25.sql
@@ -1,4 +1,4 @@
-create table q25 (id int not null AUTO_INCREMENT, answer int, primary key (id));
+create table q25 (id int not null , answer int, primary key (id));
 insert into q25 values (1,1);
 insert into q25 values (2,2);
 insert into q25 values (3,3);
